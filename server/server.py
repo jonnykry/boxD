d0ac91ec9b989a7aac8e35379484b05ff423de6b
Samuel Jackson <samjxn@iastate.edu>
2016-11-10 13:51:29 -0600

    make the demo server a little more functional (now a chat server)

--- server/server.py
+++ server/server.py
@@ -3,32 +3,58 @@ import tornado.web
 import tornado.websocket
 import tornado.template
 
+# WS_HANDLERS maintains a list of currently-opened ws connections
+WS_HANDLERS = []
+
+
 class MainHandler(tornado.web.RequestHandler):
-  def get(self):
-    loader = tornado.template.Loader(".")
-    self.write(loader.load("../client/index.html").generate())
+    def get(self):
+        loader = tornado.template.Loader(".")
+        self.write(loader.load("../client/index.html").generate())
+
 
 class WSHandler(tornado.websocket.WebSocketHandler):
-  def check_origin(self, origin):
-    return True
 
-  def open(self):
-    print 'connection opened...'
-    self.write_message("The server says: 'Hello'. Connection was accepted.")
+    def check_origin(self, origin):
+        return True
 
-  def on_message(self, message):
-    self.write_message("The server says: " + message + " back at you")
-    print 'received:', message
+    def open(self):
+        print 'connection opened.'
+        WS_HANDLERS.append(self)
+        self.write_message("The server says Hello")
 
-  def on_close(self):
-    print 'connection closed...'
+    def on_message(self, message):
+
+        # iterate over our list of connections and forward the message to each
+        for ws_handler in WS_HANDLERS:
+
+            # if the connection we're about to send to is the one who sent the message
+            if ws_handler is self:
+                name = "You"  # refer to the client as "you"
+            else:
+                name = "A client"  # refer to the client as "A client"
+
+            ws_handler.write_message("{} said:  {}".format(name, message))
+
+        print 'message received:  {}'.format(message)
+
+    def on_close(self):
+        WS_HANDLERS.remove(self)  # remove the closed connection from our list of handlers
+
+        # let the other clients know someone left
+        for ws_handler in WS_HANDLERS:
+            ws_handler.write_message('somebody left')
+
+        print 'connection closed...'
+
 
 application = tornado.web.Application([
-  (r'/ws', WSHandler),
-  (r'/', MainHandler),
-  (r"/(.*)", tornado.web.StaticFileHandler, {"path": "./resources"}),
+    (r'/ws', WSHandler),
+    (r'/', MainHandler),
+    (r"/(.*)", tornado.web.StaticFileHandler, {"path": "./resources"}),
 ])
 
 if __name__ == "__main__":
-  application.listen(9090)
-  tornado.ioloop.IOLoop.instance().start()
+    print 'starting'
+    application.listen(9090)
+    tornado.ioloop.IOLoop.instance().start()
